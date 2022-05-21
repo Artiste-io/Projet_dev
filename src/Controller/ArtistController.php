c6ff31870b47f20615e7d77df6dab392d377bdd6
ChandlerEN <chandler.nguyen@ynov.com>
2022-05-22 00:25:36 +0200

    Add Bootstrap

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArtistController extends AbstractController
 {
     #[Route('/artist', name: 'app_artist')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+//        $artist = $doctrine->getRepository(User::class)->findAll();
+
         return $this->render('artist/index.html.twig', [
             'controller_name' => 'ArtistController',
+//            'artist' => $artist,
         ]);
     }
 }
