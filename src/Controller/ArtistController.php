4aa18c8aff1baad07e8b346522ef19734d41be7c
ChandlerEN <chandler.nguyen@ynov.com>
2022-05-24 11:20:59 +0200

    Rendu final

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -15,28 +15,28 @@ class ArtistController extends AbstractController
     #[Route('/artist', name: 'app_artist')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $artist = $doctrine->getRepository(User::class)->findBy(['profil' => 'Artist']);
-//        $artist = $doctrine->getRepository(User::class)->findBy(['artist' => notnull]);
+        $artist = $doctrine->getRepository(User::class)->findBy(
+            ['profil' => 'Artist']
+        );
 
         for($i = 0; $i < sizeof($artist); $i++){
             $tab[$i][0] = $artist[$i];
-//            $tab[$i][1] = $artist[$i]->getTags();
-
-            $tags2 = $artist[$i]->getArtist();
-            $tab[$i][1] = $tags2->getTags();
+            if($artist[$i]->getArtist() == null)
+            {
+                $tab[$i][1] = null;
+                $tab[$i][2] = null;
+            }
+            else
+            {
+                $tags2 = $artist[$i]->getArtist();
+                $tab[$i][1] = $tags2->getTags();
+                $tab[$i][2] = $tags2->getGalerie();
+            }
         }
-//        $tags1 = $artist[0]->getArtist();
-//        $tags = $tags1->getTags();
-
-        $gal = $artist[0]->getArtist();
-        $galerie = $gal->getGalerie();
-
-//        $artist = $artist[1]->getId();
 
         return $this->render('artist/index.html.twig', [
             'controller_name' => 'ArtistController',
             'artist' => $tab,
-            'tags' => $galerie,
         ]);
     }
 }
